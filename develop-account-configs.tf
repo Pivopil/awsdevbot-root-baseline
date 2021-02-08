1fef0bcf835b7d46367cdb72a8d80a42fbb09c41
Pivopil <myworkplace8@gmail.com>
2021-02-08 20:48:09 +0200

    Shared domain with dev aws account

--- develop-account-configs.tf
+++ develop-account-configs.tf
@@ -1,6 +1,10 @@
 variable "develop_assume_role" {
 }
 
+locals {
+  develop_account_domain = "dev.${var.public_subdomain}"
+}
+
 provider "aws" {
   alias = "develop"
   assume_role {
@@ -13,5 +17,16 @@ resource "aws_s3_bucket" "develop-s3" {
   bucket_prefix = "develop-s3-bucket-"
 }
 
+resource "aws_route53_zone" "develop_route53_zone" {
+  provider = aws.develop
+  name = local.develop_account_domain
+}
 
+resource "aws_route53_record" "develop_route53_ns_record" {
+  zone_id = data.aws_route53_zone.public.zone_id
+  name    = local.develop_account_domain
+  type    = "NS"
+  ttl     = "30"
+  records = aws_route53_zone.develop_route53_zone.name_servers
+}
 
