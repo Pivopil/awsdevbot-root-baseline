a05c060ddd2393a1d7ab8d2c2e64005959a75b12
Pivopil <myworkplace8@gmail.com>
2021-03-15 20:34:50 +0200

    Added audit account

---
+++ audit-account-config.tf
@@ -0,0 +1,18 @@
+variable "audit_account_id" {
+}
+
+locals {
+  audit_assume_role    = "arn:aws:iam::${var.audit_account_id}:role/OrganizationAccountAccessRole"
+}
+
+provider "aws" {
+  alias = "audit"
+  assume_role {
+    role_arn = local.audit_assume_role
+  }
+}
+
+resource "aws_s3_bucket" "audit_test_bucket" {
+  provider = aws.audit
+  name = "audit-test-ghfj85"
+}
